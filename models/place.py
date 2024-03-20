43be2af78d72f03c322cb4c73cc909842dbcc753
Therese-Claire <awonguagabi@gmail.com>
2024-03-20 05:07:04 -0700

    DBStorage - Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String,\
+        ForeignKey, MetaData, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 import models
+
+
+metadata = Base.metadata
+place_amenity = Table(
+    'place_amenity', metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), PrimaryKey=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           PrimaryKey=True)
+)
 
 
 class Place(BaseModel, Base):
@@ -24,6 +35,8 @@ class Place(BaseModel, Base):
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete, delete-orphan',
                            backref='place')
+    amenities = relationship('Amenity', secondary='place_amenity',
+                             viewonly='False')
 
     @property
     def reviews(self):
@@ -39,3 +52,14 @@ class Place(BaseModel, Base):
             if instance.place_id == self.id:
                 result.append(instance)
         return result
+
+    @property
+    def amenities(self):
+        """ returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place """
+        return self.amenity_ids
+
+    @amenities.setter
+    def amenities(sel, obj=None):
+        if isinstance(obj, Amenity) and obj.id not in amenity_ids:
+            self.amenity_ids.append(obj.id)
