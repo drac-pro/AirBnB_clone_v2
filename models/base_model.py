ca908277e43e4050cd9359cf25bf292edd38a44a
Therese-Claire <awonguagabi@gmail.com>
2024-03-20 18:00:57 -0700

    Python Unittests

--- models/base_model.py
+++ models/base_model.py
@@ -23,17 +23,23 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
         else:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
-                if key != '__class__':
-                    setattr(self, key, value)
             if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
+                kwargs['id'] = str(uuid.uuid4())
             if 'updated_at' not in kwargs:
-                self.updated_at = self.created_at
+                kwargs['updated_at'] = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime\
+                                        .strptime(kwargs['updated_at'],
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' not in kwargs:
+                kwargs['created_at'] = datetime.now()
+            else:
+                kwargs['created_at'] = datetime\
+                                        .strptime(kwargs['created_at'],
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
