0a8158e2ec6a1ad5fbf3d98000442365aac4df54
Darius Tohtin <dariustohtin@gmail.com>
2024-04-04 19:30:55 -0700

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,51 @@
 #!/usr/bin/python3
 """Fabric script (based on the file 2-do_deploy_web_static.py) that creates
 and distributes an archive to your web servers"""
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+from datetime import datetime
+from fabric.api import local, env, put, run
+import os
 
+
+env.user = 'ubuntu'
+env.hosts = ['52.4.1.57', '52.3.244.12']
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    if not os.path.isdir('versions'):
+        os.makedirs('versions')
+    file = "versions/web_static_{}.tgz".\
+        format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    local("tar -czvf {} web_static".format(file))
+    if not os.path.exists(file):
+        return None
+    return file
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers
+    Args:
+        archive_path(string)
+    Returns:
+        true otherwise false
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    file_ext = os.path.basename(archive_path)
+    file_name = file_ext.split('.')[0]
+    remote_path = '/data/web_static/releases/{}/'.format(file_name)
+
+    try:
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}".format(remote_path))
+        run('tar -xzf /tmp/{} -C {} --strip-components=1'.format
+            (file_ext, remote_path))
+        run('rm /tmp/{}'.format(file_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(remote_path))
+        return True
+    except Exception:
+        return False
 
 def deploy():
     """creates and distributes an archive to your web servers"""
