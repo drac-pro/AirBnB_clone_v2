64adcaaa0abef5696d3ffda8430f15ee3ff2a503
Darius Tohtin <dariustohtin@gmail.com>
2024-04-19 19:40:01 +0000

    edit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -26,7 +26,7 @@ def python(text):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-	return '{} is a number'.format(n)
+        return '{} is a number'.format(n)
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
