62b666c91cb41ce533f8eed6b9176494584b21bb
Darius Tohtin <dariustohtin@gmail.com>
2024-04-19 20:14:32 +0000

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -27,6 +27,10 @@ def python(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
         return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+	return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
