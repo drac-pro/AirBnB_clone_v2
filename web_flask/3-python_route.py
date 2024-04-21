91bf7d7b66fc4f8d85e4091fe9fd6ed823b32323
Darius Tohtin <tohtindarius@gmail.com>
2024-04-21 11:45:59 -0700

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,21 +8,29 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-        return 'Hello HBNB!'
+    """a simple web flask"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-        return 'HBNB'
+    """a simple web flask"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-        text = text.replace('_', ' ')
-        return 'C ' + text
+    """a simple web flask"""
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-	return 'Python ' + text.replace('_', ' ')
+    """a simple web flask"""
+    return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
